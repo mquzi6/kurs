acf6d2bf7f531c84ba71bd9427132e2dcbaab21f
Yaroslav Petrov <yaroslavpetrov2312@gmail.com>
2026-05-05 20:31:50 +0300

    Initial commit6

--- kurs/kurs/settings.py
+++ kurs/kurs/settings.py
@@ -50,8 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'kurs.urls'
 
 TEMPLATES = [
